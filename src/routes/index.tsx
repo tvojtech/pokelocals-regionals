b589819efdd9467ca30a50546301e091caece402
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-03-28 11:23:00 +0100

    redirect from homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: Home,
-})
-
-function Home() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!!!</h3>
-    </div>
-  )
-}
+export const Route = createFileRoute("/")({
+  loader() {
+    return redirect({ to: "/players" });
+  },
+});
